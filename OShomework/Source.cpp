ca5325b1589b17e22cd9a74a226a441c9856db6e
WangHao <poopoo1993@gmail.com>
2018-04-27 00:02:42 +0800

    add timer for each question, but it show 0 for each.

--- OShomework/Source.cpp
+++ OShomework/Source.cpp
@@ -1,22 +1,28 @@
 #include "function.h"
 #include <sys/types.h>
 #include <stdio.h>
-#include <unistd.h>
+#include <unistd.h> //fork
+#include <ctime> //time
 
  int main(void) {	
 	string fileName = "output.txt";
 	fout.open(fileName.c_str(),ios::out);
+	clock_t start, stop;
+
 	//Q1 start
 	fout<<"Q1 : bubble sort with single thread."<<endl;
 	fileName = "input1";
 	openFile(fileName);
 	vector <int> data;
 	readFile(data);
+	start = clock();//timer start
 	bubbleSort(data);
+	stop = clock();//timer stop
 	printData(data);
 	data.clear();
 	fin.close();
 	fout<<"End of Q1"<<endl<<endl;
+	double timeforQ1 = (stop-start) / CLOCKS_PER_SEC;
 	//Q1 end
 
 	//Q2 strat
@@ -32,7 +38,7 @@
 	int *rc = new int[parts];
 	pthread_t *thread = new pthread_t [parts];
 
-
+	start = clock();//timer start
 	for (int i = 0; i < parts; i++) {
 		rc[i] = pthread_create(&thread[i], NULL, threadFn, &dividedData[i]);
 		if (rc[i] == 0) {
@@ -45,6 +51,7 @@
 	}
 
 	mergeSort(dividedData, data, parts);
+	stop = clock();//timer stop
 	printData(data);
 	data.clear();
 	for(int i = 0; i < parts; i++){
@@ -52,19 +59,18 @@
 	}
 	fin.close();
 	fout<<"End of Q2"<<endl<<endl;
+	double timeforQ2 = (stop-start) / CLOCKS_PER_SEC;
 	//Q2 end
 
 	/*Q3
-	string fileName;
+	string fileName = "input3";
 	openFile(fileName);
-	vector <int> data;
 	readFile(data);
-	vector <int> *dividedData;
-	int parts;
+
 	divideData(data, dividedData, parts);
 	int* status = 0;
 	pid_t *pid=new pid_t[parts];	
-
+	start = clock();
 	for (int i = 0; i < parts; i++) {
 		vector<int> *v;
 		v = &dividedData[i];
@@ -79,10 +85,17 @@
 		}
 	}
 
-	sleep(5);
-	
+	//wait all child process to end.
 	mergeSort(dividedData,data,parts);
+	stop = clock();
 	printData(data);
+	data.clear();
+        for(int i = 0; i < parts; i++){
+                dividedData[i].clear();
+        }
+        fin.close();
+        fout<<"End of Q3"<<endl<<endl;
+        double timeforQ3 = (stop-start) / CLOCKS_PER_SEC;	
 	*/
 	
 	//Q4 start
@@ -92,15 +105,22 @@
 	readFile(data);
 
 	divideData(data, dividedData, parts);
+	start = clock();//timer start
 	for (int i = 0; i < parts; i++) {
 		bubbleSort(dividedData[i]);
 	}
 	mergeSort(dividedData, data, parts);
+	stop = clock();//timer stop
 	printData(data);
 	fin.close();
 	fout<<"End of Q4."<<endl<<endl;
+	double timeforQ4 = (stop-start) / CLOCKS_PER_SEC; 
 	//Q4 end
 
-
-	//fout execute time to output.
-}
+	fout<<"Execute time :"<<endl;
+	fout<<"Q1 : "<<timeforQ1<<endl;
+	fout<<"Q2 : "<<timeforQ2<<endl;
+	//fout<<"Q3 : "<<timeforQ3<<endl;
+	fout<<"Q4 : "<<timeforQ4<<endl;
+	
+}	
